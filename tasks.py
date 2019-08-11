20cda2f49f3575b65e53e29a2d18ac786b286426
Marc Cataford <c.marcandre@gmail.com>
2019-08-11 11:18:00 -0400

    wip: parametrize some calls, avoid redundant hardcoded args

--- tasks.py
+++ tasks.py
@@ -3,6 +3,8 @@ from invoke import task
 class NoDeviceFoundException(Exception):
     pass
 
+SKETCH_PATH = './src/LukeWarmBeer'
+
 def _find_board(ctx):
     board_list = ctx.run('bin/arduino-cli board list', hide='stdout')
     lines = board_list.stdout.split('\n')
@@ -30,7 +32,7 @@ def upload(ctx):
     try:
         # We probably should not sudo.
         device_port = _find_board(ctx)
-        ctx.run('sudo bin/arduino-cli upload -p {port} -b arduino:avr:uno ./src/LukeWarmBeer'.format(port=device_port))
+        ctx.run('sudo bin/arduino-cli upload -p {port} -b arduino:avr:uno {path}'.format(port=device_port, path=SKETCH_PATH))
     except NoDeviceFoundException:
         print('No device found')
     except Exception as e:
@@ -38,4 +40,4 @@ def upload(ctx):
 
 @task
 def compile(ctx):
-    ctx.run('bin/arduino-cli compile -b arduino:avr:uno ./src/LukeWarmBeer')
+    ctx.run('bin/arduino-cli compile -b arduino:avr:uno {path}'.format(path=SKETCH_PATH))
