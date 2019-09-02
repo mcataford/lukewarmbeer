4837ec369236edad360d6169c09b2674daaef6e8
Marc Cataford <c.marcandre@gmail.com>
2019-09-02 15:11:12 -0400

    wip: direction decision based on analog input

--- src/LukeWarmBeer/LukeWarmBeer.ino
+++ src/LukeWarmBeer/LukeWarmBeer.ino
@@ -1,17 +1,49 @@
-int LED_PIN = 13;
-int SWITCH_PIN = 8;
+/*
+ * Pin mapping:
+ * A0: Joystick A
+ * A1: Joystick B
+ * 
+ */
+
+int DIRECTION_THRESHOLD = 512;
+int TOLERANCE = 10;
+int ANALOG_READ_MAX = 1023;
+
+int a0 = A0;
+
+int joystick_A = 0;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(SWITCH_PIN, INPUT);
+  // For easy debugging.
+  Serial.begin(9600);
+}
+
+int get_direction(int joystick_reading) {
+  // If the value is invalid, ignore.
+  if (joystick_reading < 0 || joystick_reading > ANALOG_READ_MAX)
+    return 0;
+  
+  int low_tol = DIRECTION_THRESHOLD - TOLERANCE;
+  int high_tol = DIRECTION_THRESHOLD + TOLERANCE;
+
+  if (joystick_reading < low_tol)
+    return -1;
+  else if (joystick_reading > high_tol) {
+    return 1;  
+  } else {
+    return 0;  
+  }
 }
 
 void loop() {
-    int switch_state = digitalRead(SWITCH_PIN);
+    joystick_A = analogRead(A0);
 
-    if (switch_state) {
-        digitalWrite(LED_PIN, HIGH);
-    } else {
-        digitalWrite(LED_PIN, LOW);
-    }
+    int direction_A = get_direction(joystick_A);
+
+    Serial.print("Reading: ");
+    Serial.println(joystick_A);
+    Serial.print("Direction: ");
+    Serial.println(direction_A);
+
+    delay(500);
 }
