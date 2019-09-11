15c6e5fcb90cb6c7093d068ccd53a6c574b314b7
Phil Isaac <isaac.phil@gmail.com>
2019-09-11 13:18:51 -0400

    Marc: connect motors with joysticks

--- src/LukeWarmBeer/LukeWarmBeer.ino
+++ src/LukeWarmBeer/LukeWarmBeer.ino
@@ -7,59 +7,80 @@
  * 
  */
 
-int DIRECTION_THRESHOLD = 512;
-int TOLERANCE = 10;
-int ANALOG_READ_MAX = 1023;
+int joystick_A_UP = 13;
+int joystick_A_DOWN = 12;
+int joystick_B_UP = 11;
+int joystick_B_DOWN = 10;
 
-int joystick_A_pin = A0;
-int joystick_B_pin = A1;
 int motor_A_direction_pin = 4;
 int motor_A_step_pin = 3;
 
-int joystick_A = 0;
-int joystick_B = 0;
+int motor_B_direction_pin = 2;
+int motor_B_step_pin = 5;
 
 void setup() {
-  // For easy debugging.
   Serial.begin(9600);
-
+  
   pinMode(motor_A_direction_pin, OUTPUT);
   pinMode(motor_A_step_pin, OUTPUT);
 
+  pinMode(motor_B_direction_pin, OUTPUT);
+  pinMode(motor_B_step_pin, OUTPUT);
+  
+  pinMode(joystick_A_UP, INPUT);
+  pinMode(joystick_A_DOWN, INPUT);
+  pinMode(joystick_B_UP, INPUT);
+  pinMode(joystick_B_DOWN, INPUT);
+  
   // This sets up the PWM to drive the step pin.
   analogWrite(motor_A_step_pin, 128);
 }
 
-int get_direction(int joystick_reading) {
-  // If the value is invalid, ignore.
-  if (joystick_reading < 0 || joystick_reading > ANALOG_READ_MAX)
-    return 0;
-  
-  int low_tol = DIRECTION_THRESHOLD - TOLERANCE;
-  int high_tol = DIRECTION_THRESHOLD + TOLERANCE;
+void setMotor(int motorID, int direction) {
+  int target_direction_pin;
+  int target_step_pin;
 
-  if (joystick_reading < low_tol)
-    return -1;
-  else if (joystick_reading > high_tol) {
-    return 1;  
+  if (motorID == 1) {
+    target_direction_pin = motor_A_direction_pin;
+    target_step_pin = motor_A_step_pin;
+  } else if (motorID == 2) {
+    target_direction_pin = motor_B_direction_pin;
+    target_step_pin = motor_B_step_pin;
+  }
+  
+  if (direction == 0) {
+    analogWrite(target_step_pin, 0);
   } else {
-    return 0;  
+    analogWrite(target_step_pin, 128);
+  }
+
+  if (direction > 0) {
+    digitalWrite(target_direction_pin, HIGH);
+  } else {
+    digitalWrite(target_direction_pin, LOW);
   }
 }
 
 void loop() {
-    joystick_A = analogRead(A0);
-    joystick_B = analogRead(A1);
-    
-    int direction_A = get_direction(joystick_A);
-    int direction_B = get_direction(joystick_B);
-
-    // Setting the direction pin based on joystick_A value.
-    if (direction_A > 0) {
-      digitalWrite(motor_A_direction_pin, HIGH);
+    boolean A_up = digitalRead(joystick_A_UP);
+    boolean A_down = digitalRead(joystick_A_DOWN);
+    boolean B_up = digitalRead(joystick_B_UP);
+    boolean B_down = digitalRead(joystick_B_DOWN);
+        
+    if (A_up && !A_down) {
+      setMotor(1, 1);
+    } else if (A_down && !A_up) {
+      setMotor(1, -1);
+    } else {
+      setMotor(1, 0);
     }
-    else if (direction_A < 0) {
-      digitalWrite(motor_A_direction_pin, LOW);
+    
+    if (B_up && !B_down) {
+      setMotor(2, 1);
+    } else if (B_down && !B_up) {
+      setMotor(2, -1);
+    } else {
+      setMotor(2, 0);
     }
  
     delay(100);
