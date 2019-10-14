718f8f66fcd5a27a589601fdd535c916b96e8878
Phil Isaac <isaac.phil@gmail.com>
2019-10-13 20:26:40 -0400

    allow setting motor speeds

--- src/LukeWarmBeer/LukeWarmBeer.ino
+++ src/LukeWarmBeer/LukeWarmBeer.ino
@@ -1,12 +1,12 @@
 const int L_DOWN = 9;
 const int L_UP = 8;
 const int R_DOWN = 12;
-const int R_UP = 10;
+const int R_UP = 13;
 
 const int L_MOTOR_DIRECTION = 4;
-const int L_MOTOR_STEP = 5;
+const int L_MOTOR_STEP = 10;
 const int R_MOTOR_DIRECTION = 7;
-const int R_MOTOR_STEP = 6;
+const int R_MOTOR_STEP = 3;
 
 void setup() {
   pinMode(L_MOTOR_DIRECTION, OUTPUT);
@@ -18,6 +18,41 @@ void setup() {
   pinMode(L_UP, INPUT_PULLUP);
   pinMode(R_DOWN, INPUT_PULLUP);
   pinMode(R_UP, INPUT_PULLUP);
+
+  configureLeftMotor();
+  configureRightMotor();
+}
+
+void configureLeftMotor() {
+  // timer 1, output B
+  // fast PWM with top limit set by OCR1A
+  // clock prescale starts at max: 1024
+  TCCR1A = _BV(COM1B1) | _BV(WGM11) | _BV(WGM10);
+  TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS12) | _BV(CS10);
+  OCR1A = 255;
+  OCR1B = 128;
+}
+
+void configureRightMotor() {
+  // timer 2, ouput B
+  // fast PWM with top limit set by OCR2A
+  // clock prescale starts at max: 1024
+  TCCR2A = _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+  TCCR2B = _BV(WGM22) | _BV(CS22) | _BV(CS21) | _BV(CS20);
+  OCR2A = 255;
+  OCR2B = 128;
+}
+
+void setLeftSpeed(int count) {
+  // use timer 1, output B, pin 10
+  OCR1A = count;
+  OCR1B = count / 2;
+}
+
+void setRightSpeed(int speed) {
+  // use timer 2, output B, pin 3
+  OCR2A = count;
+  OCR2B = count / 2;
 }
 
 void loop() {
@@ -46,6 +81,4 @@ void loop() {
     } else {
       analogWrite(R_MOTOR_STEP, 0);
     }
-
-    delay(100);
 }
