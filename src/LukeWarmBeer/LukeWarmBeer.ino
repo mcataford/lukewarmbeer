a2cdb49bf802f9a6f51a9b59d69fc0b7eca22192
Phil Isaac <isaac.phil@gmail.com>
2019-11-10 22:51:05 -0500

    add display logic

--- src/LukeWarmBeer/LukeWarmBeer.ino
+++ src/LukeWarmBeer/LukeWarmBeer.ino
@@ -1,12 +1,34 @@
+// Constants
+const int OFF = 0;
+const int ON = 1;
+
+// Inputs
 const int L_DOWN = 9;
 const int L_UP = 8;
-const int R_DOWN = 12;
-const int R_UP = 13;
+const int R_DOWN = 1;
+const int R_UP = 0;
 
+// Motor outputs
 const int L_MOTOR_DIRECTION = 4;
 const int L_MOTOR_STEP = 10;
 const int R_MOTOR_DIRECTION = 7;
 const int R_MOTOR_STEP = 3;
+
+// MAX7219 LED Driver pins
+const int MAX7219_DIN = 6;
+const int MAX7219_CLK = 13;
+const int MAX7219_CS = 12;
+
+// Register values. See MAX7219 Datasheet, Table 2, page 7
+const int MAX7219_REG_DECODE = 0x09;
+const int MAX7219_REG_INTENSITY = 0x0A;
+const int MAX7219_REG_SCANLIMIT = 0x0B;
+const int  MAX7219_REG_SHUTDOWN = 0x0C;
+const int MAX7219_REG_DISPTEST = 0x0F;
+
+/******************************************************************************
+  Setup
+ */
 
 void setup() {
   pinMode(L_MOTOR_DIRECTION, OUTPUT);
@@ -21,6 +43,8 @@ void setup() {
 
   configureLeftMotor();
   configureRightMotor();
+
+  configureDisplay();
 }
 
 void configureLeftMotor() {
@@ -43,19 +67,65 @@ void configureRightMotor() {
   OCR2B = 128;
 }
 
-void setLeftSpeed(int count) {
-  // use timer 1, output B, pin 10
-  OCR1A = count;
-  OCR1B = count / 2;
+void configureDisplay() {
+  pinMode(MAX7219_DIN, OUTPUT);
+  pinMode(MAX7219_CS, OUTPUT);
+  pinMode(MAX7219_CLK, OUTPUT);
+  digitalWrite(MAX7219_CS, HIGH);
+
+  resetDisplay();
 }
 
-void setRightSpeed(int speed) {
-  // use timer 2, output B, pin 3
-  OCR2A = count;
-  OCR2B = count / 2;
+void resetDisplay()
+{
+    setRegister(MAX7219_REG_SHUTDOWN, OFF);   // turn off display
+    setRegister(MAX7219_REG_DISPTEST, OFF);   // turn off test mode
+    setRegister(MAX7219_REG_INTENSITY, 0x0D); // display intensity
 }
+
+/******************************************************************************
+  Loop
+ */
 
 void loop() {
+  String score = "01234567";
+
+  setRegister(MAX7219_REG_SHUTDOWN, OFF); // turn off
+  setRegister(MAX7219_REG_SCANLIMIT, 7); // limit to 8 digits
+  setRegister(MAX7219_REG_DECODE, 0b11111111); // decode all digits
+  setRegister(1, score.charAt(0));
+  setRegister(2, score.charAt(1));
+  setRegister(3, score.charAt(2));
+  setRegister(4, score.charAt(3));
+  setRegister(5, score.charAt(4));
+  setRegister(6, score.charAt(5));
+  setRegister(7, score.charAt(6));
+  setRegister(8, score.charAt(7));
+  setRegister(MAX7219_REG_SHUTDOWN, ON); // turn on
+
+  /* setRegister(MAX7219_REG_DISPTEST, OFF); // disp test for debugging */
+
+  delay(1000);
+
+    /* int switch_state = digitalRead(SWITCH_PIN); */
+
+    /* if (switch_state) { */
+    /*     digitalWrite(LED_PIN, HIGH); */
+    /* } else { */
+    /*     digitalWrite(LED_PIN, LOW); */
+    /* } */
+}
+
+
+void setRegister(byte reg, byte value)
+{
+    digitalWrite(MAX7219_CS, LOW);
+    shiftOut(MAX7219_DIN, MAX7219_CLK, MSBFIRST, reg);
+    shiftOut(MAX7219_DIN, MAX7219_CLK, MSBFIRST, value);
+    digitalWrite(MAX7219_CS, HIGH);
+}
+
+void controlMotors() {
     boolean l_down = !digitalRead(L_DOWN);
     boolean l_up = !digitalRead(L_UP);
 
@@ -81,4 +151,16 @@ void loop() {
     } else {
       analogWrite(R_MOTOR_STEP, 0);
     }
+}
+
+void setLeftSpeed(int count) {
+  // use timer 1, output B, pin 10
+  OCR1A = count;
+  OCR1B = count / 2;
+}
+
+void setRightSpeed(int count) {
+  // use timer 2, output B, pin 3
+  OCR2A = count;
+  OCR2B = count / 2;
 }
